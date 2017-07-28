c6b5eec59d5742d0ef14da97894e88b5924b3da7
Brian Hudi <bhudi@rightpoint.com>
2017-07-27 23:51:53 -0500

    added webpack-dev-server and editor config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,22 @@
-const path = require('path');
- 
+const path = require("path");
+
 module.exports = {
-  context: path.join(__dirname, 'src'),
-  entry: [
-    './main.js',
-  ],
+  context: path.join(__dirname, "src"),
+  entry: ["./main.js"],
   output: {
-    path: path.join(__dirname, 'www'),
-    filename: 'bundle.js',
+    path: "/",
+    filename: "bundle.js"
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
-      },
-    ],
+        use: ["babel-loader"]
+      }
+    ]
   },
   resolve: {
-    modules: [
-      path.join(__dirname, 'node_modules'),
-    ],
-  },
+    modules: [path.join(__dirname, "node_modules")]
+  }
 };
