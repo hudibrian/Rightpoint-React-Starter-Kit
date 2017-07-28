051ef0478673fd4f64004e6f5a156dc1ecc42fe0
Brian Hudi <bhudi@rightpoint.com>
2017-07-28 11:57:24 -0500

    fixed bug

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,13 +4,10 @@ import reducers from "../reducers";
 
 function configureStore() {
   const enhancer = compose(
-    autoRehydrate(),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
   const store = createStore(reducers, enhancer);
-
-  persistStore(store);
 
   return store;
 }
